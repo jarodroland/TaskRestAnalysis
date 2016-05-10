8b5d45c49a261c00e639295c93f6c572e5ef9bc3
Jarod Roland <jarod.roland@gmail.com>
2016-05-10 17:42:54 -0500

    clean-up analysis

--- RestingCorrelations.m
+++ RestingCorrelations.m
@@ -2,16 +2,18 @@
 
 %% Setup
 % define data files
-% % Pt 139 (PT55) data files
-ptNumber = 133;
-restingSamplingRate = 1200;
+% Pt 133 (PT__) data
+% ptNumber = 133;
+% restingSamplingRate = 1200;
+
+% % Pt 139 (PT55) data
 % dataDir = 'E:\Data\ECoG Task-Rest\139\';
 % metaDataFile = [dataDir 'Task\PT55_ReachingTask_DataStructure.mat'];
 % kinematicsDataFile = [dataDir 'Task\PT55_ReachingTask3D_Contra_Kinematics_All.mat'];
 % trialsDataFile = [dataDir 'Task\PT55_ReachingTask3D_Contra_QuestionableTrials_Move_and_Spectra_All.mat'];
 % restingDataFile = [dataDir 'Rest\121004-B5-7-INV_7E_EDF_seg2.mat'];
 
-% % Pt 145 (PT__) data files
+% % Pt 145 (PT__) data
 % restingSamplingRate = 512;      %MAGICNUMBER: 512Hz sampling rate for clinical ECoG system
 
 % dataDir = 'E:\Data\ECoG Task-Rest\145\';
@@ -29,27 +31,28 @@ restingDataFile = [dataDir 'Rest\' subjectID '_RestingECoGData.mat'];
 outDataFile = [dataDir 'Rest\RestingCorrelations.mat'];
 
 
-numChannels = 64;               % MAGICNUMBER: only use first 64 channels in each task file
+% numChannels = 64;               % MAGICNUMBER: only use first 64 channels in each task file
 channelToPlotList = [8 24];     % MAGICNUMBER: electrodes 8 & 24 are in motor for Pt 139
 
 
 % load meta data
 metaData = load(metaDataFile, 'DataStructure');
 metaData = metaData.DataStructure;
-numChannels = min(numChannels, max(metaData.Channels));
-channels = metaData.Channels(metaData.Channels <= numChannels);     % limit to numChannels
+% numChannels = length(metaData.Channels);
+% channelToElectrodeMap = metaData.Channels;     % maps channel number to electrode number for Task Data
 carGroups = metaData.CARgroups;
 
 % define variables
 saveData = struct();
-samplingRate = metaData.samplefreq;
+% samplingRate = metaData.samplefreq;   % task data sampling rate (not necessarily the resting data sampling rate)
 numCARGroups = size(carGroups, 2);
 freqList = 2.^(1:.125:8);                   % exponential frequency distribution
 % freqList = 2.^(1:.25:8);                    % exponential frequency distribution
 
 saveData.subjectID = subjectID;
 saveData.restingDataFile = restingDataFile;
-
+saveData.restingSamplingRate = restingSamplingRate;
+saveData.freqList = freqList;
 
 % % Nick's frequency and span lists
 % freqList = [1, 1.3748, 1.8593, 2.4754, 3.2467, 4.198, 5.3545, 6.7417, 8.3839, 10.3043, 12.5237, 15.0601, 17.9281, 21.1386, 24.6979, 28.6084, 32.8676, 37.4689, 42.4012, 47.6496, 71.3898, 77.8856, 84.5496, 91.3519, 98.2627, 105.2527, 133.4449, 140.4231, 147.3279, 154.1397, 160.8411, 167.4164, 192.2115, 197.9879, 203.5827, 208.9922];
@@ -60,22 +63,25 @@ saveData.restingDataFile = restingDataFile;
 frequencyIndexToPlot = [10 46];       % frequency index at 10 & 100Hz for Carl's list (used w/ Zac's Gabor)
 
 
-%% Common average re-referencing by channel groups
-% group our channels (by amplifier) to common-average reference within each group
-carGroupsSansNoisy = cell(numCARGroups, 1);
-for carGroup = 1:numCARGroups      % average and re-reference the signal by group
-    carGroups{carGroup}(carGroups{carGroup} > numChannels) = [];                        % limit to numChannels
-    noisyBooleanIndices = ismember(carGroups{carGroup}, metaData.NoisyChannels);        % index of noisy channels
-    carGroupsSansNoisy{carGroup} = carGroups{carGroup};                               	% make a copy to edit
-    carGroupsSansNoisy{carGroup}(noisyBooleanIndices) = [];                             % remove noisy channels from CAR groups
-end
-
-%% Pre-Process Resting Signal
+%% Load Resting Signal Data
 restingSignal = load(restingDataFile, 'signal');
 restingSignal = double(restingSignal.signal);
 % restingSignalFile = load(restingDataFile, 'signalList');
 % restingSignal = double(restingSignalFile.signalList{1});            %TODO: loop through all resting signals
 signalLen = size(restingSignal, 1);
+numChannels = size(restingSignal, 2);
+
+
+%% Pre-Process Resting Signal
+% Common average re-referencing by channel groups
+% group our channels (by amplifier) to common-average reference within each group
+carGroupsSansNoisy = cell(numCARGroups, 1);
+for carGroup = 1:numCARGroups      % average and re-reference the signal by group
+%     carGroups{carGroup}(carGroups{carGroup} > numChannels) = [];                        % limit to numChannels
+    noisyBooleanIndices = ismember(carGroups{carGroup}, metaData.NoisyChannels);        % index of noisy channels
+    carGroupsSansNoisy{carGroup} = carGroups{carGroup};                               	% make a copy to edit
+    carGroupsSansNoisy{carGroup}(noisyBooleanIndices) = [];                             % remove noisy channels from CAR groups
+end
 
 % normalize signal by CAR groups
 meanRestingSignalCARGroups = cell(numCARGroups, 1);
@@ -95,10 +101,11 @@ mainsNotchFilter120 = designfilt('bandstopiir', 'FilterOrder', 2, 'HalfPowerFreq
 restingSignalReRefNotched = filtfilt(mainsNotchFilter60, restingSignalReRef);
 restingSignalReRefNotched = filtfilt(mainsNotchFilter120, restingSignalReRefNotched);
 
+
 %% BLP correlation analysis
 % Compute BLP (band-limited power), i.e. slow rhythms of power envelope
 saveData.blpCorrelations = struct.empty;
-bandPassFrequencyList = [8, 10; 8, 13; 13, 30; 35, 50; 70, 100; 100, 140]';
+bandPassFrequencyList = [1, 4; 4, 8; 8, 10; 8, 13; 13, 30; 35, 50; 70, 85; 70, 100; 90, 105; 100, 140]';
 for bandPassFrequencies = bandPassFrequencyList
     bandPassFilter = designfilt('bandpassiir', 'FilterOrder', 4, 'HalfPowerFrequency1', bandPassFrequencies(1), 'HalfPowerFrequency2', bandPassFrequencies(2), 'SampleRate', restingSamplingRate);
     restingSignalReRefNotchedBandPass = filtfilt(bandPassFilter, restingSignalReRefNotched);
@@ -109,13 +116,13 @@ for bandPassFrequencies = bandPassFrequencyList
 
     % low pass filter the envelope
     BLPFrequencies = [0.1 1];
-    % lowPassFilter = designfilt('lowpassiir', 'FilterOrder', 4, 'PassbandFrequency', BLPFrequencies(2), 'SampleRate', samplingRate);
+    % lowPassFilter = designfilt('lowpassiir', 'FilterOrder', 4, 'PassbandFrequency', BLPFrequencies(2), 'SampleRate', restingSamplingRate);
     lowPassFilter = designfilt('bandpassiir', 'FilterOrder', 4, 'HalfPowerFrequency1', BLPFrequencies(1), 'HalfPowerFrequency2', BLPFrequencies(2), 'SampleRate', restingSamplingRate);
     restingBandPassEnvelopeFiltered = filtfilt(lowPassFilter, restingBandPassEnvelope);
 
     % compute correlation matrix 
-    restingCorrelationMatrix = corrcoef(restingBandPassEnvelopeFiltered(:, channels));     % limit only to useful channels (electrodes)
-%     restingCorrelationMatrix = corrcoef(restingBandPassEnvelopeFiltered);     % analyze all channels (prune useful channels later)
+%     restingCorrelationMatrix = corrcoef(restingBandPassEnvelopeFiltered(:, channels));     % limit only to useful channels (electrodes)
+    restingCorrelationMatrix = corrcoef(restingBandPassEnvelopeFiltered);     % analyze all channels (prune useful channels later)
 
     % save the data to disc
     saveData.blpCorrelations(end+1).bandPassFrequencies = bandPassFrequencies;
@@ -124,37 +131,37 @@ for bandPassFrequencies = bandPassFrequencyList
     % free up some memory
     % clear('restingSignal', 'restingSignalReRef', 'restingSignalReRefNotched', 'restingSignalReRefNotchedBandPass', 'restingBandPassEnvelope');
 
-    % plot the correlation matrix
-    channelToPlotList = [8 24];
-    for channelToPlot = channelToPlotList
-        figCorrelationMatrix = figure();
-        colorMapWhiteMiddle = jet();
-        colorMapWhiteMiddle(30:35, :) = repmat([1, 1, 1], 6, 1);    % white out the middle 6 indices
-
-%         rValid = restingCorrelationMatrix(channelToPlot, :);        % use max relative to the individual channel
-        rValid = restingCorrelationMatrix(channelToPlotList, :);    % use max relative to channels being plotted
-%         rValid = restingCorrelationMatrix;                          % use max of all channels
-        rValid = rValid(rValid~=1);                                 % exclude auto-correlation from max correlation
-        cLimMax = max(abs(rValid));
-        cLimMin = -cLimMax;
-        % cLimMin = min(abs(restingCorrelationMatrix(channelToPlot, :)));
-        % cLimMax = max(abs(restingCorrelationMatrix(channelToPlot, :)));
-        imagesc(flipud(rot90(reshape(restingCorrelationMatrix(channelToPlot, :), 8, 8))), [cLimMin cLimMax]) % flip and rotate to get orientation of 1-8 left-to-righ on top row
-        colormap(colorMapWhiteMiddle);
-        colorbar()
-        fileOut = sprintf('%sFigures\\Correlation Matrix - %s - FreqBand %03i-%03iHz - Seed Ch %02i', dataDir, subjectID, bandPassFrequencies(1), bandPassFrequencies(2), channelToPlot);
-        print(figCorrelationMatrix, [fileOut '.png'],  '-dpng');    % save figure as PNG
-        savefig(figCorrelationMatrix, [fileOut '.fig']);               % save figure as FIG
-        close(figCorrelationMatrix);
-
-        % % plot a sample channel 
-        % figure();
-        % hold on;
-        % plot(restingSignalReRefNotchedBandPass((40000:60000), channelToPlot), 'r');
-        % plot(restingBandPassEnvelope((40000:60000), channelToPlot), 'b');
-        % plot(restingBandPassEnvelopeFiltered((40000:60000), channelToPlot), 'k');
-        % hold off;
-    end
+%     % plot the correlation matrix
+%     channelToPlotList = [8 24];
+%     for channelToPlot = channelToPlotList
+%         figCorrelationMatrix = figure();
+%         colorMapWhiteMiddle = jet();
+%         colorMapWhiteMiddle(30:35, :) = repmat([1, 1, 1], 6, 1);    % white out the middle 6 indices
+% 
+% %         rValid = restingCorrelationMatrix(channelToPlot, :);        % use max relative to the individual channel
+%         rValid = restingCorrelationMatrix(channelToPlotList, :);    % use max relative to channels being plotted
+% %         rValid = restingCorrelationMatrix;                          % use max of all channels
+%         rValid = rValid(rValid~=1);                                 % exclude auto-correlation from max correlation
+%         cLimMax = max(abs(rValid));
+%         cLimMin = -cLimMax;
+%         % cLimMin = min(abs(restingCorrelationMatrix(channelToPlot, :)));
+%         % cLimMax = max(abs(restingCorrelationMatrix(channelToPlot, :)));
+%         imagesc(flipud(rot90(reshape(restingCorrelationMatrix(channelToPlot, :), 8, 8))), [cLimMin cLimMax]) % flip and rotate to get orientation of 1-8 left-to-righ on top row
+%         colormap(colorMapWhiteMiddle);
+%         colorbar()
+%         fileOut = sprintf('%sFigures\\Correlation Matrix\\%s-Freq%03i-%03iHz-Seed%02i', dataDir, subjectID, bandPassFrequencies(1), bandPassFrequencies(2), channelToPlot);
+%         print(figCorrelationMatrix, [fileOut '.png'],  '-dpng');    % save figure as PNG
+%         savefig(figCorrelationMatrix, [fileOut '.fig']);               % save figure as FIG
+%         close(figCorrelationMatrix);
+% 
+%         % % plot a sample channel 
+%         % figure();
+%         % hold on;
+%         % plot(restingSignalReRefNotchedBandPass((40000:60000), channelToPlot), 'r');
+%         % plot(restingBandPassEnvelope((40000:60000), channelToPlot), 'b');
+%         % plot(restingBandPassEnvelopeFiltered((40000:60000), channelToPlot), 'k');
+%         % hold off;
+%     end
 end
 
 save(outDataFile, '-struct', 'saveData');
